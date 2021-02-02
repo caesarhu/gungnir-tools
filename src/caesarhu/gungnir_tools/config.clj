ed6dadefc3a0d12364168cba9d404a5693261b28
caesarhu <hupeishun@gmail.com>
2021-02-03 05:33:32 +0800

    fix config.clj all def to defonce

--- src/caesarhu/gungnir_tools/config.clj
+++ src/caesarhu/gungnir_tools/config.clj
@@ -1,16 +1,16 @@
 (ns caesarhu.gungnir-tools.config
   (:require [malli.core :as m]))
 
-(def translate-key*
+(defonce translate-key*
   (atom :locale/zh-tw))
 
-(def postgres-keys*
+(defonce postgres-keys*
   (atom {:type-key :postgres/type
          :column-call-set (set [:primary-key :unique :default :references])}))
 
-(def ragtime-key*
+(defonce ragtime-key*
   (atom :ragtime/id))
 
-(def malli-type-keys*
+(defonce malli-type-keys*
   (atom (set (concat (keys (m/type-schemas))
                      [:local-date :local-date-time]))))
